fbf4a07f498cd93782bf22410f8fc044ea4b0105
krzy-sok <01178595@pw.edu.pl>
2025-04-23 19:51:29 +0200

    feat: added mockup of docstring_options and docstring_style attributes

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,5 +1,7 @@
+from src.mkdocstrings_handlers.go._internal import config
+
 def test_collect_module(handler) -> None:
     """Assert existing module can be collected."""
     identifier = "github.com/gin-gonic/gin"
-    handler.collect(identifier) 
+    handler.collect(identifier, config.GoOptions())
     assert handler._collected[identifier] is not None
