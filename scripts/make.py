06c9be9659ce02c73b79aa73d920073524b1b761
krzy-sok <01178595@pw.edu.pl>
2025-04-27 14:24:47 +0200

    fix: make godocjson ruff pass

--- scripts/make.py
+++ scripts/make.py
@@ -123,7 +123,7 @@ def vscode() -> None:
 def godocjson() -> None:
     if platform.system() == "Windows":
         subprocess.run("./install_golang.ps1", "-version", "1.24.2") # noqa: S603, PLW1510
-        subprocess.run(["C:\go1.24.2\\bin\go.exe", "install", "github.com/rtfd/godocjson@latest"])  # noqa: S603, PLW1510, S607
+        subprocess.run(["C:\\go1.24.2\\bin\\go.exe", "install", "github.com/rtfd/godocjson@latest"])  # noqa: S603 ,PLW1510
     subprocess.run(["/bin/rm", "-rf", "/usr/local/go"])     # noqa: S603, PLW1510
     subprocess.run(["/bin/wget", "https://golang.org/dl/go1.24.2.linux-amd64.tar.gz"])  # noqa: S603, PLW1510
     subprocess.run(["/bin/sudo", "/usr/bin/tar", "-C", "/usr/local",  "-xzf",  "go1.24.2.linux-amd64.tar.gz"])  # noqa: S603, PLW1510
