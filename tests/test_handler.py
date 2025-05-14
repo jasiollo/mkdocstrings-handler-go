67abffeda8ac189c743b270e51e2337079223067
krzy-sok <01178595@pw.edu.pl>
2025-05-14 11:07:24 +0200

    tests: collect function with 2 return values, function with struct param

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -71,6 +71,104 @@ func Foo(b int) int{
     }
 
 
+def test_collect_function_tuple_return(tmp_path: pathlib.Path, handler: handler.GoHandler) -> None:
+    file_str = """
+package main
+
+//  foo bar function
+// this does not newline in dockcomment
+//
+//	there go the arguments
+//
+// something something description
+func Foo(b int) (int, int){
+	return b
+}
+"""
+    f = tmp_path / "bar.go"
+    f.write_text(file_str, encoding="utf-8")
+
+    handler.collect(str(tmp_path / "bar.go"), config.GoOptions())
+    print(handler._collected)
+    assert handler._collected[str(tmp_path/ "bar.go")] == {
+        "type": "package",
+        "doc": "",
+        "name": "main",
+        "importPath": str(tmp_path),
+        "imports": [],
+        "filenames": [str(tmp_path / "bar.go")],
+        "notes": {},
+        "bugs": None,
+        "consts": [],
+        "types": [],
+        "vars": [],
+        "funcs": [
+            {
+                "doc": " foo bar function\nthis does not newline in dockcomment\n\n\tthere go the arguments\n\nsomething something description\n",
+                "name": "Foo",
+                "packageName": "main",
+                "packageImportPath": str(tmp_path),
+                "type": "func",
+                "filename": str(tmp_path / "bar.go"),
+                "line": 10,
+                "parameters": [{'type': 'int', 'name': 'b'}],
+                "results": [{'type': 'int', 'name':''}, {'type': 'int', 'name':''}],
+                "recv": "",
+                "orig": "",
+            },
+        ],
+    }
+
+
+def test_collect_function_unnanmed_struct_arg(tmp_path: pathlib.Path, handler: handler.GoHandler) -> None:
+    file_str = """
+package main
+
+//  foo bar function
+// this does not newline in dockcomment
+//
+//	there go the arguments
+//
+// something something description
+func NewHouse(opts struct{Material string; HasFireplace bool; Composable bool; Floors int}){
+    return
+}
+"""
+    f = tmp_path / "bar.go"
+    f.write_text(file_str, encoding="utf-8")
+
+    handler.collect(str(tmp_path / "bar.go"), config.GoOptions())
+    print(handler._collected)
+    assert handler._collected[str(tmp_path/ "bar.go")] == {
+        "type": "package",
+        "doc": "",
+        "name": "main",
+        "importPath": str(tmp_path),
+        "imports": [],
+        "filenames": [str(tmp_path / "bar.go")],
+        "notes": {},
+        "bugs": None,
+        "consts": [],
+        "types": [],
+        "vars": [],
+        "funcs": [
+            {
+                "doc": " foo bar function\nthis does not newline in dockcomment\n\n\tthere go the arguments\n\nsomething something description\n",
+                "name": "NewHouse",
+                "packageName": "main",
+                "packageImportPath": str(tmp_path),
+                "type": "func",
+                "filename": str(tmp_path / "bar.go"),
+                "line": 10,
+                "parameters": [{'type': 'struct{string,bool,bool,int}', 'name': 'opts'}],
+                "results": [],
+                "recv": "",
+                "orig": "",
+            },
+        ],
+    }
+
+
 def test_collect_with_package_dock(tmp_path: pathlib.Path, handler: handler.GoHandler) -> None:
     file_str = """
 //package does stuff
