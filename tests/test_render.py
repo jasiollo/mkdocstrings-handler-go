522db61ca5db153272ab349a234fb03314145b8d
krzy-sok <01178595@pw.edu.pl>
2025-05-21 18:47:04 +0200

    test: test render package

--- tests/test_render.py
+++ tests/test_render.py
@@ -3,6 +3,7 @@ import re
 from mkdocstrings_handlers.go._internal import config, handler
 
 
+# obsolete with root=true in render
 def test_render_function(handler: handler.GoHandler) -> None:
     res = handler.render(
         {
@@ -18,45 +19,9 @@ def test_render_function(handler: handler.GoHandler) -> None:
             "recv": "",
             "orig": "",
         },
-        config.GoOptions(show_symbol_type_heading=True),
+        config.GoOptions(show_symbol_type_heading=True, show_root_heading=True),
     )
-    html = """
-    <div class="doc doc-object doc-function">
-        <h2 id="./bar.go" class="doc doc-heading">
-            <code class="doc-symbol doc-symbol-heading doc-symbol-function"></code>
-            <span class="doc doc-object-name doc-object-function-name">Foo</span>
-        </h2>
-        <div class="doc-signature highlight">
-            <pre>
-            <span></span>
-                <code>
-                <span class="nx">Foo</span>
-                <span class="p">(</span>
-                <span class="w"></span>
-                <span class="nx">b</span>
-                <span class="w"></span>
-                <span class="kt">int</span>
-                <span class="p">,</span>
-                <span class="w"></span>
-                <span class="nx">c</span>
-                <span class="w"></span>
-                <span class="kt">int</span>
-                <span class="p">)</span>
-                <span class="w"></span>
-                <span class="kt">int</span>\n
-                </code>
-            </pre>
-        </div>
-        <div class="doc doc-contents ">
-            foo bar function
-            this does not newline in dockcomment
-            there go the arguments
-
-            something something description
-
-
-        </div>
-    </div>"""
+    html = '<div class="doc doc-object doc-function"></div>'
     expected = re.sub(r"\n\s*", "", html)
     res = re.sub(r">\s+<", r"><", re.sub(r"\n\s*", "", res))
     assert res == expected
@@ -65,7 +30,7 @@ def test_render_function(handler: handler.GoHandler) -> None:
 def test_render_package(handler: handler.GoHandler) -> None:
     package_json = {
         "type": "package",
-        "doc": "",
+        "doc": "package doc",
         "name": "main",
         "importPath": "/some/path",
         "imports": [],
@@ -92,11 +57,15 @@ def test_render_package(handler: handler.GoHandler) -> None:
         ],
     }
 
-    res = handler.render(package_json, config.GoOptions(show_symbol_type_heading=True))
+    res = handler.render(package_json, config.GoOptions(show_symbol_type_heading=True, show_root_heading=True))
 
     html = """
     <div class="doc doc-object doc-module">
-        <div class="doc doc-contents ">
+        <h2 id="/some/path" class="doc doc-heading">
+            <code>/some/path/main</code>
+        </h2>
+        <div class="doc doc-contents first">
+            package doc
             <div class="doc doc-object doc-function">
                 <h3 id="/some/path/bar.go" class="doc doc-heading">
                     <code class="doc-symbol doc-symbol-heading doc-symbol-function"></code>
